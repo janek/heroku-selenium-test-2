7adc3d1bd45d2325a9a3d2fff65eeab38fef9039
Janek Szynal <jan.szynal@gmail.com>
2021-11-01 07:23:26 +0100

    x

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+service = Service(os.environ.get("CHROMEDRIVER_PATH"))
+driver = webdriver.Chrome(chrome_options=chrome_options, service=service)
 
 # Now you can start using Selenium
